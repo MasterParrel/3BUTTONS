f6ef33e5a8e68d55900ba8a74db63e47a4a46fbc
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2023-05-26 15:19:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -104,43 +104,25 @@ class Bot(Client):
                 )
                 sys.exit()
                 
-        if FORCE_SUB_GROUP3:
-
-            try:
-
+       if FORCE_SUB_GROUP3:
+           try:
                 link = (await self.get_chat(FORCE_SUB_GROUP3)).invite_link
-
                 if not link:
-
                     await self.export_chat_invite_link(FORCE_SUB_GROUP3)
-
                     link = (await self.get_chat(FORCE_SUB_GROUP3)).invite_link
-
                 self.invitelink4 = link
-
-            except Exception as a:
-
-                self.LOGGER(__name__).warning(a)
-
-                self.LOGGER(__name__).warning(
-
-                    "Bot tidak dapat Mengambil link invite dari FORCE_SUB_GROUP2!"
-
-                )
-
-                self.LOGGER(__name__).warning(
-
-                    f"Pastikan @{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_GROUP2}"
-
-                )
-
-                self.LOGGER(__name__).info(
-
-                    "Bot Berhenti. Gabung Group https://t.me/ramsupportt untuk Bantuan"
-
-                )
-
-                sys.exit()
+           except Exception as a:
+               self.LOGGER(__name__).warning(a)
+               self.LOGGER(__name__).warning(
+                   "Bot tidak dapat mengambil link invite dari FORCE_SUB_GROUP3!"
+               )
+               self.LOGGER(__name__).warning(
+                   f"pastikan@{self.username} adalah admin di Channel Tersebut, Chat ID F-Subs Channel Saat Ini: {FORCE_SUB_GROUP3}"
+               )
+               self.LOGGER(__name__).info(
+                   "Bot Berhenti. Gabung Group https://t.me/sioyenbul untuk bantuan"
+               )
+               sys.exit()
 
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
